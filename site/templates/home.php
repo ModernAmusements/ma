be90be63c89f75e7f7ca3aa3e4bcf921b35982df
ModernAmusements <st@zahnarzt-helden.de>
2021-02-01 12:17:39 +0100

    homepage rework

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,49 +1,40 @@
 <?php snippet('header') ?>
 <main>
-  <article id="pov" class="pageContent">
-    <section class="homeSvg card ">
-      <svg id="homeSVG" class="stroke image" viewBox="0 0 1250 640" xmlns="http://www.w3.org/2000/svg">
-        <path d="M.47,472V179.61H638.31V80.83h-86V.47h697.18V527.11H1109.09V477H756.33V639.53H238.61V558.72h29.92V472Z" />
-        <div class="screen" onClick=""></div>
-      </svg>
-    </section>
-    <section class="homeSvg-mobile card">
-      <svg class="stroke image" viewBox="0 0 687 1342" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M196 1.00024L498.5 1.00026L498.5 685.277H599.861V593.058H686V1341H121.5L121.5 1160.5H175.254L175.254 811.895L1 811.895L1.00002 240.911H87.6265V273.008H196L196 1.00024Z" />
-        <div class="screen" onClick=""></div>
-      </svg>
-
-    </section>
-    <section class="credentialsName">
-      <h1 class="displayText">
-        Shady Nathan Tawfik
-        <br />
-       <h1>Junior FullStack Web-Dev</h1>
-      </h1>
-    </section>
-    <section class="credentialsTime">
-      <h1 class="displayText">
-        2016
-        <br />
-        -2021
-      </h1>
-    </section>
-    <section class="credentialsDate">
-    <p class="subheadline">
-    technical capabilities:</p>
-      <h1 class="x-large">
-        php 7 / Type/JavaScript(Es6)
-        <br />
-        Laravel / VueJS
-        <br />
-        Web Server / Authentication
-        <br />
-        nginx / AUTH0 2.0
-      </h1>
+  <article class="pageContent">
+    <section class="right">
+      <?php if ($worksPage = page('works')): ?>
+      <?php foreach ($worksPage->children()->sortBy('date', 'desc') as $work) :
+      ?>
+      <article class="post-homepage">
+        <div class="post-homepage-header">
+          <span class="cat">Projects</span>
+          <span><a href="/" class="showpost">
+            Show more
+          </a></span>
+        </div>
+        <div class="post-homepage-content">
+   
+          <?php if ($cover = $work->cover()->crop(200)) : ?>
+            <div class="post-homepage-cover">
+            <img src="<?= $cover->url() ?>" alt="<?= $cover->alt() ?>" />
+            </div>
+            <?php endif ?>
+            <div class="post-homepage-title">
+            <h3><?= $work->title() ?></h3>
+            <p class="small">
+            We decided to express our gratitude by sharing a retrospective of our first year as a studio
+            </p>
+            </div>
+        </div>
+     
+      </article>
+      <?php endforeach ?>
+      <?php endif ?>
+      
     </section>
   </article>
-</main>
-<div id="preload-homepage">
+
+  <!-- <div id="preload-homepage">
   <div class="intro">
     <div class="intro-items">
       <h1 class="large">Florian Nagel</h1>
@@ -53,6 +44,7 @@
       <span class="intro-enter medium">(Click to Enter)</span>
     </div>
   </div>
-</div>
+</div>  -->
+</main>
 <?= js('assets/js/homePageScripts.js', true) ?>
 <?php snippet('footer') ?>
